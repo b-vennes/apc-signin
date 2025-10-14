e3fb11e245e76b84c194999633f83d7df83e31ef
Branden Vennes <b.n.vennes@gmail.com>
2025-10-14 17:27:31 -0500

    improves the sign in form a bit

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -2,10 +2,32 @@
 
 import Link from "next/link";
 import Modal from "@/components/modal.tsx";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
+
+function getInputText(event: FormEvent<HTMLInputElement>): string {
+  return (event.target as unknown as { value: string }).value;
+}
+
+function getInputCheckbox(
+  event: unknown,
+): boolean {
+  const eventWithChecked = event as { target: { checked: boolean } };
+
+  return eventWithChecked?.target?.checked ?? false;
+}
 
 export default function SignIn() {
   const [agreementOpen, setAgreementOpen] = useState(false);
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [agreed, setAgreed] = useState(false);
+
+  const submitEnabled = name &&
+    name !== "" &&
+    email &&
+    email !== "" &&
+    agreed;
 
   return (
     <div
@@ -29,13 +51,17 @@ export default function SignIn() {
           <input
             id="full-name-input"
             type="text"
-            className="border-2 rounded-sm md:w-1/2 bg-slate-200 focus:bg-slate-50"
+            className="border-2 rounded-sm md:w-1/2 px-2 py-1"
+            placeholder="Keith Elwin"
+            onInput={(input) => setName(getInputText(input))}
           />
           <label id="email-label">Email</label>
           <input
             id="email-input"
             type="email"
-            className="border-2 rounded-sm md:w-1/2 bg-slate-200 focus:bg-slate-50"
+            className="border-2 rounded-sm md:w-1/2 px-2 py-1"
+            placeholder="keith.elwin@hotmail.com"
+            onInput={(input) => setEmail(getInputText(input))}
           />
         </div>
         <div
@@ -50,33 +76,49 @@ export default function SignIn() {
             <button
               type="button"
               id="view-agreement-button"
-              className="bg-slate-500 rounded-sm px-4 py-1"
-              onClick={() => {
-                setAgreementOpen(true);
-                console.log("open agreement clicked");
-              }}
+              className="not-active:border-l-1 bg-amber-100 border-amber-400 rounded-sm px-4 py-1 hover:cursor-pointer hover:bg-amber-200"
+              onClick={() => setAgreementOpen(true)}
             >
               View
             </button>
           </div>
           <div className="flex flex-row gap-1">
-            <input id="member-agreement-checkbox" type="checkbox" />
-            <label id="agree-checkbox" className="text-sm">
+            <input
+              id="member-agreement-checkbox"
+              type="checkbox"
+              onClick={(event) => setAgreed(getInputCheckbox(event))}
+            />
+            <label
+              id="agree-checkbox"
+              className="text-sm"
+            >
               I agree to the rules of the Austin Pinball Collective space set
               forth in the Austin Pinball Collective Member Agreement
             </label>
           </div>
         </div>
         <div>
-          <button
-            type="submit"
-            className="bg-slate-500 rounded-sm px-4 py-1 text-lg"
-            onClick={() => {
-              window.location.href = "/signin-success";
-            }}
-          >
-            Submit
-          </button>
+          {submitEnabled
+            ? (
+              <button
+                type="submit"
+                className="bg-emerald-400 not-active:border-l-2 border-emerald-700 rounded-sm px-4 py-1 text-lg hover:cursor-pointer hover:bg-emerald-500"
+                onClick={() => {
+                  window.location.href = "/signin-success";
+                }}
+              >
+                Submit
+              </button>
+            )
+            : (
+              <button
+                type="submit"
+                className="bg-slate-200 border-slate-300 border-l-2 rounded-sm px-4 py-1 text-lg text-slate-50 hover:cursor-not-allowed"
+                disabled
+              >
+                Submit
+              </button>
+            )}
         </div>
       </div>
       {agreementOpen
